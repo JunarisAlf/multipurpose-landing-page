cb24a607e4939646c2670d240e48fd09fd358764
JunarisAlf <junaris0715@gmail.com>
2022-10-10 17:26:14 +0700

    Finish Hero Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: ["./src/pages/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    plugins: [require('flowbite/plugin')],
+    content: ['./src/pages/**/*.{html,js}', './node_modules/flowbite/**/*.js'],
+    theme: {
+        
+        fontFamily: {
+            sans: ['Poppins', 'sans-serif'],
+        },
+        extend: {
+            colors: {
+                transparent: 'transparent',
+                current: 'currentColor',
+                'orange': '#FF5300',
+                'orange-soft': '#FFF7F4',
+                'black-soft': '#373737',
+                'dark-strong': '#18191A',
+                'dark-mid': '#242526',
+                'dark-soft': '#3A3B3C',
+                'gray': '#7B7B7B',
+            }
+        },
+    },
+    plugins: [],
+};
